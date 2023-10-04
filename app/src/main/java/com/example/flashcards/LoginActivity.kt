1494f707a3192ccfbc85ef0dbeef6ae2a4cc2aad
Mustafa <mustafataibah@gmail.com>
2023-10-04 14:56:52 -0400

    login page for the app, missing functionality for the login button to navigate to the next page

---
+++ app/src/main/java/com/example/flashcards/LoginActivity.kt
@@ -0,0 +1,29 @@
+package com.example.flashcards
+
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+
+            } else {
+                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
