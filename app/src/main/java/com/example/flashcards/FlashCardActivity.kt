177411a60f3a08939282291832fcfbd7084e7631
Mustafa <mustafataibah@gmail.com>
2023-10-04 18:56:09 -0400

    functional app, TODO: savestate for rotation

--- app/src/main/java/com/example/flashcards/FlashCardActivity.kt
+++ app/src/main/java/com/example/flashcards/FlashCardActivity.kt
@@ -3,13 +3,16 @@ package com.example.flashcards
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
 
 class FlashCardActivity : AppCompatActivity() {
 
-    var problems = ArrayList<Problem>()
-    var score = 0
+    private var problems = ArrayList<Problem>()
+    private var score = 0
+    private var currentProblemIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,44 +21,74 @@ class FlashCardActivity : AppCompatActivity() {
         val welcomeToast = Toast.makeText(this, "Welcome ${intent.getStringExtra("username")}", Toast.LENGTH_SHORT)
         welcomeToast.show()
 
-        if (savedInstanceState != null) {
-            // Save Logic
-        }
-
         val generateButton = findViewById<Button>(R.id.generateButton)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        val answerEditText = findViewById<EditText>(R.id.answerEditText)
+
         generateButton.setOnClickListener {
-            generateProblems()
-            // Disable button after generating problems
+            problems = Problem.generateProblems() as ArrayList<Problem>
+            currentProblemIndex = 0
+            displayProblem()
             generateButton.isEnabled = false
         }
 
-        val submitButton = findViewById<Button>(R.id.submitButton)
         submitButton.setOnClickListener {
-            checkAnswers()
-            generateButton.isEnabled = true
+            val userAnswer = answerEditText.text.toString().toIntOrNull()
+
+            if (userAnswer != null && userAnswer == problems[currentProblemIndex].getAnswer()) {
+                score++
+            }
+
+            currentProblemIndex++
+
+            if (currentProblemIndex >= problems.size) {
+                Toast.makeText(this, "Score: $score out of 10", Toast.LENGTH_LONG).show()
+                generateButton.isEnabled = true
+            } else {
+                displayProblem()
+            }
+        }
+    }
+
+    private fun displayProblem() {
+        val currentProblem = problems[currentProblemIndex]
+
+        val operand1TextView = findViewById<TextView>(R.id.operand1TextView)
+        val operand2TextView = findViewById<TextView>(R.id.operand2TextView)
+        val operatorTextView = findViewById<TextView>(R.id.operatorTextView)
+        val answerEditText = findViewById<EditText>(R.id.answerEditText)
+
+        operand1TextView.text = currentProblem.operand1.toString()
+        operand2TextView.text = currentProblem.operand2.toString()
+        operatorTextView.text = if (currentProblem.isAddition) "+" else "-"
+        answerEditText.text.clear()
+    }
+
+    private data class Problem(val isAddition: Boolean, val operand1: Int, val operand2: Int) {
+        fun getAnswer(): Int {
+            return if (isAddition) operand1 + operand2 else operand1 - operand2
         }
 
+        companion object {
+            fun generateProblems(): List<Problem> {
+                val problems = mutableListOf<Problem>()
+
+                val half = 5
+                for (i in 0 until half) {
+                    val operand1 = Random.nextInt(1, 100)
+                    val operand2 = Random.nextInt(1, 21)
+                    problems.add(Problem(true, operand1, operand2))
+                }
+
+                for (i in 0 until half) {
+                    val operand1 = Random.nextInt(1, 100)
+                    val operand2 = Random.nextInt(1, 21)
+                    problems.add(Problem(false, operand1, operand2))
+                }
+
+                problems.shuffle()
+                return problems
+            }
+        }
     }
-
-    private fun generateProblems() {
-//        problems.clear()
-//        for (i in 0 until 10) {
-//            val operand1 = Random.nextInt(1, 100)
-//            val operand2 = Random.nextInt(1, 21)
-//            problems.add(Problem(operand1, operand2))
-//        }
-//        displayProblems()
-    }
-
-    private fun displayProblems() {
-        // Display Logic
-
-    }
-
-    private fun checkAnswers(){
-        score = 0
-        // Check Logic
-    }
-
-//    override fun onSaveInstanceState(outState: Bundle) {}
 }
