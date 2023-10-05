81a510dffcce2f42ac511491a8624d74e4fad6ea
Mustafa <mustafataibah@gmail.com>
2023-10-05 11:58:07 -0400

    Readability Changes

--- app/src/main/java/com/example/flashcards/FlashCardActivity.kt
+++ app/src/main/java/com/example/flashcards/FlashCardActivity.kt
@@ -6,7 +6,6 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.random.Random
 
 
 class FlashCardActivity : AppCompatActivity() {
@@ -37,7 +36,6 @@ class FlashCardActivity : AppCompatActivity() {
             problems = ArrayList(problemsArray?.map { Problem.fromInt(it) } ?: listOf())
             score = savedInstanceState.getInt("score")
             currentProblemIndex = savedInstanceState.getInt("currentProblemIndex")
-//            problems = savedInstanceState.getSerializable("problems") as ArrayList<Problem>
 
             if (problems.isNotEmpty()) {
                 displayProblem()
@@ -103,55 +101,5 @@ class FlashCardActivity : AppCompatActivity() {
         outState.putIntArray("problemsArray", problemsArray)
         outState.putInt("score", score)
         outState.putInt("currentProblemIndex", currentProblemIndex)
-//        outState.putSerializable("problems", problems)
-    }
-
-    internal data class Problem(val isAddition: Boolean, val operand1: Int, val operand2: Int) {
-        fun getAnswer(): Int {
-            return if (isAddition) operand1 + operand2 else operand1 - operand2
-        }
-
-        fun toInt() : Int {
-            val op1 = String.format("%02d", operand1)
-            val op2 = String.format("%02d", operand2)
-
-            val operationInt = if (isAddition) 0 else 1
-
-            return "$op1$op2$operationInt".toInt()
-        }
-
-
-
-
-        companion object {
-            fun fromInt(intInput: Int): Problem {
-                val strValue = intInput.toString()
-
-                val operand1 = strValue.substring(0, 2).toInt()
-                val operand2 = strValue.substring(2, 4).toInt()
-                val isAddition = strValue.last() == '0'
-
-                return Problem(isAddition, operand1, operand2)
-            }
-            fun generateProblems(count: Int ): List<Problem> {
-                val problems = mutableListOf<Problem>()
-                val half = count / 2
-
-                for (i in 0 until half) {
-                    val operand1 = Random.nextInt(1, 100)
-                    val operand2 = Random.nextInt(1, 21)
-                    problems.add(Problem(true, operand1, operand2))
-                }
-
-                for (i in 0 until half) {
-                    val operand1 = Random.nextInt(1, 100)
-                    val operand2 = Random.nextInt(1, 21)
-                    problems.add(Problem(false, operand1, operand2))
-                }
-
-                problems.shuffle()
-                return problems
-            }
-        }
     }
 }
